35e95ce87221d23920babe872f402018aeae585b
Gaetan Redoutez <redoutezgaetan@gmail.com>
2024-09-20 11:20:04 +0200

    feat(modal) : add close modal function and event

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const modalCloseBtn = document.querySelector(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -20,4 +21,12 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+/**
+ * Close modal function
+ */
+const closeModal = () => {
+  modalbg.style.display = "none";
+};
 
+// Close modal event
+modalCloseBtn.addEventListener("click", closeModal);
